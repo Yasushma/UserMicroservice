9e4e5ce743986b5993aef92884da1029520f88be
Pa1-234 <150997748+Pa1-234@users.noreply.github.com>
2023-12-11 14:01:04 +0530

    Create kops.sh

---
+++ kops.sh
@@ -0,0 +1,11 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+chmod +x kops-linux-amd64 kubectl
+mv kubectl /usr/local/bin/kubectl
+mv kops-linux-amd64 /usr/local/bin/kops
+
+aws s3api create-bucket --bucket pocmicroservices1.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket pocmicroservices1.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://pocmicroservices1.k8s.local
+kops create cluster --name pocmicroservicescluster.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name pocmicroservicescluster.k8s.local --yes
